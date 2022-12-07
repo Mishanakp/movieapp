97ae7695b215587292e7ab6b62cc45bb319f56d4
mishana <mishanakp.official@gmail.com>
2022-12-07 09:19:11 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./components/Header/Header.js";
+import Home from "./components/Home/Home.js";
+import PageNotFound from "./components/PageNotFound/PageNotFound.js";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
+import Footer from "./components/Footer/Footer";
+import "./App.scss";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header></Header>
+          <div className="container">
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/movie/:imdbID" exact component={MovieDetail} />
+                {/* <Route component={PageNotFound} /> */}
+            </Switch>
+          </div>
+        <Footer />
+      </Router>
     </div>
   );
 }
