119073fb0ececf845ebeb61468ef4683c72bd71c
mishana <mishanakp.official@gmail.com>
2022-12-08 16:10:02 +0530

    added error message function

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./components/Header/Header.js";
 import Home from "./components/Home/Home.js";
-import PageNotFound from "./components/PageNotFound/PageNotFound.js";
 import MovieDetail from "./components/MovieDetail/MovieDetail";
 import Footer from "./components/Footer/Footer";
 import "./App.scss";
@@ -17,7 +16,6 @@ function App() {
             <Switch>
                 <Route path="/" exact component={Home} />
                 <Route path="/movie/:imdbID" exact component={MovieDetail} />
-                <Route component={PageNotFound} />
             </Switch>
           </div>
         <Footer />
